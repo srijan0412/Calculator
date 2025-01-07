a1ee675a981c8d369cdd2bae49effe278e96408b
srijan0412 <srijan.soni01@gmail.com>
2025-01-07 10:36:36 +0530

    Second commit

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,18 @@ class Calculator {
     public int add(int op1, int op2) {
         return op1 + op2;
     }
+    public int sub(int op1, int op2){
+        return op1 - op2;
+    }
+    public int mul(int op1, int op2){
+        return op1*op2;
+    }
+    public int div(int op1, int op2){
+        if (op2 == 0){
+            return -1;
+        }
+        return op1/op2;
+    }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -21,7 +33,18 @@ class Calculator {
             System.out.println(add(Input1, Input2));
             return;
         }
-        
+        else if (Op == "sub"){
+            System.out.println(sub(Input1, Input2));
+            return;
+        }
+        else if (Op == "div"){
+            System.out.println(div(Input1, Input2));
+            return;
+        }
+        else if (Op == "mul"){
+            System.out.println(mul(Input1, Input2));
+            return;
+        }
 
     }
 }
